60fbb7f8ec0f82e634242bf5da5c3eafc87837f5
i5ting <i5ting@126.com>
2016-12-26 11:33:18 +0800

    return ctx.body not path

--- index.js
+++ index.js
@@ -106,9 +106,7 @@ function send(ctx, path, opts) {
     if (!ctx.response.get('Last-Modified')) ctx.set('Last-Modified', stats.mtime.toUTCString());
     if (!ctx.response.get('Cache-Control')) ctx.set('Cache-Control', 'max-age=' + (maxage / 1000 | 0));
     ctx.type = type(path);
-    ctx.body = fs.createReadStream(path);
-
-    return path;
+    return ctx.body = fs.createReadStream(path);
   });
 }
 
